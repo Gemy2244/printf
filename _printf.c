f3fdcd5feeb57c86a6f1eb548b32ebd5f6ea7e52
GH770 <mohmed_abdallah770@hotmail.com>
2023-08-22 05:12:13 -0700

    tasks gh770 update

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+* _printf - produces a stdout with arguments and specifiers
+* @format: the format
+* Return: returns the number of bytes printed
+*/
+
+int _printf(const char *format, ...)
+{
+	int total = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	const char *pointerToCharacter = format;
+	while (*pointerToCharacter != '\0')
+	{
+		if (*pointerToCharacter == '%')
+		{
+			pointerToCharacter++;
+			switch (*pointerToCharacter)
+			{
+				case 'c':
+					{
+						char c = (char)va_arg(args, int);
+						_putchar(c);
+						total++;
+						break;
+					}
+				case 's':
+					{
+						char *string = va_arg(args, char *);
+						write(1, string, _strlen(string));
+						total += _strlen(string);
+						break;
+					}
+				case '%':
+					{
+						_putchar('%');
+						total++;
+						break;
+					}
+				case 'd':
+				case 'i':
+					{
+						int dintiger = va_arg(args, int);
+						char *string = iTOs(dintiger);
+						write(1, string, _strlen(string));
+						total += _strlen(string);
+						free(string);
+						break;
+					}
+				case 'b':
+					{
+						unsigned int bUnsigned = va_arg(args, unsigned int);
+						char *string = inTObinary(bUnsigned);
+						write(1, string, _strlen(string));
+						total += _strlen(string);
+						free(string);
+						break;
+					}
+				default:
+					{
+						_putchar('%');
+						_putchar(*pointerToCharacter);
+						total += 2;
+						break;
+					}
+			}
+		}
+		else
+		{
+			_putchar(*pointerToCharacter);
+			total++;
+		}
+		pointerToCharacter++;
+	}
+
+	va_end(args);
+
+	return total;
+}
