551f4a10c8403aa0a843ae53f3d38247a3250ae4
Gemy2244 <me@example.com>
2023-08-22 07:29:17 -0700

    last update

--- helpingfunction.c
+++ helpingfunction.c
@@ -9,36 +9,35 @@ char *iTOs(int n)
 {
 int sign = n;
 
-    int index = 0;
-    char *newstring = malloc(sizeof(char) * 12);
-    if (newstring == NULL)
-    {
-        return NULL;
-    }
+int index = 0;
+char *newstring = malloc(sizeof(char) * 12);
+	if (newstring == NULL)
+	{
+	return (NULL);
+	}
 
 
 
-    if (n < 0)
-    {
-        n = -n;
-    }
+	if (n < 0)
+	{
+	n = -n;
+	}
 
-    do
-    {
-        newstring[index++] = n % 10 + '0';
-        n /= 10;
-    } while (n > 0);
+	do {
+	newstring[index++] = n % 10 + '0';
+	n /= 10;
+	} while (n > 0);
 
-    if (sign < 0)
-    {
-        newstring[index++] = '-';
-    }
+	if (sign < 0)
+	{
+	newstring[index++] = '-';
+	}
 
-    newstring[index] = '\0';
+	newstring[index] = '\0';
 
-    reverser(newstring, index);
+	reverser(newstring, index);
 
-    return newstring;
+	return (newstring);
 }
 
 
@@ -51,19 +50,19 @@ int sign = n;
 void reverser(char *string, int length)
 {
 
-    char *starting = string;
-    char *ending = string + length - 1;
+char *starting = string;
+char *ending = string + length - 1;
 
-    char temporarychar;
+char temporarychar;
 
-    while (starting < ending)
-    {
-        temporarychar = *starting;
-        *starting = *ending;
-        *ending = temporarychar;
-        starting++;
-        ending--;
-    }
+	while (starting < ending)
+	{
+	temporarychar = *starting;
+	*starting = *ending;
+	*ending = temporarychar;
+	starting++;
+	ending--;
+	}
 }
 
 /**
@@ -74,21 +73,20 @@ void reverser(char *string, int length)
 char *inTObinary(unsigned int n)
 {
 int index = 0;
-    char *newstring = malloc(sizeof(char) * 33);
-    if (newstring== NULL)
-    {
-        return NULL;
-    }
+char *newstring = malloc(sizeof(char) * 33);
+	if (newstring == NULL)
+	{
+	return (NULL);
+	}
 
 
-     do
-     {
-         newstring[index++] = (n & 1) + '0';
-         n >>= 1;
-     } while (n > 0);
-     newstring[index] = '\0';
-     reverser(newstring, index);
-     return newstring;
+	do {
+	newstring[index++] = (n & 1) + '0';
+	n >>= 1;
+	} while (n > 0);
+	newstring[index] = '\0';
+	reverser(newstring, index);
+	return (newstring);
 }
 
 /**
@@ -109,16 +107,18 @@ int _putchar(char c)
 *
 * Return: length of the string
 */
-unsigned int _strlen(const char *string) {
+unsigned int _strlen(const char *string)
+{
 
 unsigned int length = 0;
 
-  while (*string != '\0') {
+	while (*string != '\0')
+	{
 
-    length++;
+	length++;
 
-    string++;
-  }
+	string++;
+	}
 
-  return length;
+	return (length);
 }
